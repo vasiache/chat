f86dd26089a3c1181058431f2a1b0c33654831b9
Четвертухин Василий <v.chetvertukhin@crpt.ru>
2023-12-05 10:54:33 +0300

    change model

--- src/main.py
+++ src/main.py
@@ -5,23 +5,10 @@ from aiogram.utils import executor
 
 open_ai_token = ""
 tg_token=""
+model="gpt-3.5-turbo-16k"
 openai.api_key=open_ai_token
 bot=Bot(tg_token)
 dp=Dispatcher(bot)
-
-#chat
-#@dp.message_handler()
-#async def send(message : types.Message):
-#    response = openai.Completion.create(
-#    model="text-davinci-002",
-#    prompt=message.text,
-#    temperature=0.9,
-#    max_tokens=1000,
-#    top_p=1,
-#    frequency_penalty=0.0,
-#    presence_penalty=0.6,
-#    stop=["You"])
-#    await message.answer(response['choices'][0]['text'])
 
 @dp.message_handler()
 async def send(message : types.Message):
@@ -29,10 +16,10 @@ async def send(message : types.Message):
   bot.send_message(message.from_user.id, "Понял, ща")
   message.text=message.text[6:]
   response = openai.Completion.create(
-    model="text-davinci-003",
+    model=model,
     prompt=message.text,
     temperature=0.3,
-    max_tokens=2000,
+    max_tokens=8000,
     top_p=1.0,
     frequency_penalty=0.0,
     presence_penalty=0.6)
